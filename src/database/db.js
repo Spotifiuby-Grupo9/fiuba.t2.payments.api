98e70bf9cb06b50a812ea1618f86688c4b0ba779
jowisaidman <joelsaidman1@gmail.com>
2022-06-22 00:10:51 -0300

    feat: add wallet creation and get all

--- src/database/db.js
+++ src/database/db.js
@@ -1,33 +1,18 @@
-const { MongoClient } = require("mongodb");
+mongoose = require('mongoose');
+payment = require('./schemas/payment.js');
 
 const connection = async () => {
-  /**
-   * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
-   * See https://docs.mongodb.com/ecosystem/drivers/node/ for more details
-   */
   const uri =
-    "mongodb+srv://user:FOrKF9muEfWunnQ8@spotifiuby-g9.wrf52.mongodb.net/payments?retryWrites=true&w=majority'";
+    "mongodb+srv://user:FOrKF9muEfWunnQ8@spotifiuby-g9.wrf52.mongodb.net/payments-test?retryWrites=true";
 
-  const client = new MongoClient(uri);
+  mongoose.connect(uri);
 
   try {
-    // Connect to the MongoDB cluster
-    await client.connect();
-
-    // Make the appropriate DB calls
-    await listDatabases(client);
+    await mongoose.connect(uri);
+    return mongoose;
   } catch (e) {
     console.error(e);
-  } finally {
-    await client.close();
   }
 };
-
-async function listDatabases(client) {
-  databasesList = await client.db().admin().listDatabases();
-
-  console.log("Databases:");
-  databasesList.databases.forEach(db => console.log(` - ${db.name}`));
-}
 
 module.exports.connection = connection;
--- src/services/wallets.js
+++ src/services/wallets.js
@@ -1,5 +1,8 @@
 const ethers = require("ethers");
-const accounts = [];
+const payment = require("../database/schemas/payment.js");
+// const accounts = [];
+
+const Payment = payment.schema();
 
 const getDeployerWallet = ({ config }) => () => {
   const provider = new ethers.providers.InfuraProvider(config.network, config.infuraApiKey);
@@ -12,27 +15,35 @@ const createWallet = () => async () => {
   const provider = new ethers.providers.InfuraProvider("kovan", process.env.INFURA_API_KEY);
   // This may break in some environments, keep an eye on it
   const wallet = ethers.Wallet.createRandom().connect(provider);
-  accounts.push({
-    address: wallet.address,
-    privateKey: wallet.privateKey,
+  
+  const uid = "jasfRcNOyFZzLJjSlPPWLFAMghD2"; //TODO: remove harcoding, bring param 
+
+  const newPayment = new Payment({ 
+      address: wallet.address,
+      privateKey: wallet.privateKey,
+      uid: uid 
   });
+
+  newPayment.save().then(() => console.log('paymentAdded: ', newPayment));
+
   const result = {
-    id: accounts.length,
+    id: "TODO", //TODO get from answer
     address: wallet.address,
     privateKey: wallet.privateKey,
+    uid: uid,
   };
   return result;
 };
 
 const getWalletsData = () => () => {
-  return accounts;
+  return Payment.find();
 };
 
 const getWalletData = () => index => {
-  return accounts[index - 1];
+  return accounts[index - 1]; //TODO
 };
 
-const getWallet = ({}) => index => {
+const getWallet = ({}) => index => { //TODO
   const provider = new ethers.providers.InfuraProvider("kovan", process.env.INFURA_API_KEY);
 
   return new ethers.Wallet(accounts[index - 1].privateKey, provider);
