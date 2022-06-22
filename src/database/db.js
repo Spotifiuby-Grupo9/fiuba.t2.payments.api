8581871ded1e7c3ce5b020d8e83422923a036556
jowisaidman <joelsaidman1@gmail.com>
2022-06-21 22:51:02 -0300

    feat: add basic connection to database

---
+++ src/database/db.js
@@ -0,0 +1,35 @@
+const {MongoClient} = require('mongodb');
+
+const connection = async () => {
+    /**
+     * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
+     * See https://docs.mongodb.com/ecosystem/drivers/node/ for more details
+     */
+     const uri =
+     "mongodb+srv://user:FOrKF9muEfWunnQ8@spotifiuby-g9.wrf52.mongodb.net/payments?retryWrites=true&w=majority'";
+ 
+
+    const client = new MongoClient(uri);
+ 
+    try {
+        // Connect to the MongoDB cluster
+        await client.connect();
+ 
+        // Make the appropriate DB calls
+        await  listDatabases(client);
+ 
+    } catch (e) {
+        console.error(e);
+    } finally {
+        await client.close();
+    }
+}
+
+async function listDatabases(client){
+    databasesList = await client.db().admin().listDatabases();
+ 
+    console.log("Databases:");
+    databasesList.databases.forEach(db => console.log(` - ${db.name}`));
+};
+
+module.exports.connection = connection;
