656f48129f5563d10a58a942892a8af80a7111b5
jowisaidman <joelsaidman1@gmail.com>
2022-07-16 15:14:47 -0300

    feat: adding swagger WIP

--- src/server.js
+++ src/server.js
@@ -14,6 +14,53 @@ const port = process.env.PORT || 5000;
 // Run the server!
 const start = async () => {
   try {
+    await fastify.register(require('@fastify/swagger'), {
+      routePrefix: '/documentation',
+      swagger: {
+        info: {
+          title: 'Test swagger',
+          description: 'Testing the Fastify swagger API',
+          version: '0.1.0'
+        },
+        externalDocs: {
+          url: 'https://swagger.io',
+          description: 'Find more info here'
+        },
+        host: 'localhost',
+        schemes: ['http'],
+        consumes: ['application/json'],
+        produces: ['application/json'],
+        tags: [
+          { name: 'user', description: 'User related end-points' },
+          { name: 'code', description: 'Code related end-points' }
+        ],
+        definitions: {
+          User: {
+            type: 'object',
+            required: ['id', 'email'],
+            properties: {
+              id: { type: 'string', format: 'uuid' },
+              firstName: { type: 'string' },
+              lastName: { type: 'string' },
+              email: {type: 'string', format: 'email' }
+            }
+          }
+        },
+      },
+      uiConfig: {
+        docExpansion: 'full',
+        deepLinking: false
+      },
+      uiHooks: {
+        onRequest: function (request, reply, next) { next() },
+        preHandler: function (request, reply, next) { next() }
+      },
+      staticCSP: true,
+      transformStaticCSP: (header) => header,
+      exposeRoute: true
+    })
+    await fastify.ready()
+    fastify.swagger()
     await fastify.listen(port, "0.0.0.0");
     fastify.log.info(`server listening on ${fastify.server.address().port}`);
     await database.connection();
